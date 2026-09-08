abcb95ebc27756d1a40fccbddc8eb7c8cb3f6ac2
Kim <johnlordbelgica18@gmail.com>
2026-09-08 08:35:15 +0800

    First Commit

---
+++ app/src/main/java/com/example/group5_tasktrack/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.group5_tasktrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
